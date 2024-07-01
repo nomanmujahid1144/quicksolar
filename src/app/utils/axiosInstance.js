68474e726797d2da67cc180b72af0e8d50d530f7
nomanmujahid1144 <nomanmujahid.cs@gmail.com>
2024-07-01 19:37:43 +0500

    add: online server

--- src/app/utils/axiosInstance.js
+++ src/app/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://budcarserver.cheapify.store',
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://quicksolar-eight.vercel.app/api',
+    // baseURL: 'http://localhost:3000/api'
 });
 
 
